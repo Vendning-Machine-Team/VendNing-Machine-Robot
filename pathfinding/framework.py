ec1453587dc6c6788bc5350ec14bf554a39c96c2
SamuelSaylor <SamuelSaylor2025@gmail.com>
2025-10-22 13:31:22 -0500

    Pathfinding example framework

--- pathfinding/framework.py
+++ pathfinding/framework.py
@@ -1,6 +1,17 @@
 import cv2 as cv
 import config
-from ultralytics import yol
+from ultralytics import YOLO as yol
 
-yolo = yol.YOLO(config.PATHFINDING_MODEL_PATH)
+yolo = yol(config.PATHFINDING_MODEL_PATH)
 
+test_images = [cv.imread("pathfinding/testimage.jpg"),
+               cv.imread("pathfinding/image0.jpeg"),
+               cv.imread("pathfinding/image0 (1).jpeg")]
+
+image_scans = [yolo(img) for img in test_images]
+for i, scan in enumerate(image_scans):
+    result_img = scan[0].plot()
+    cv.imshow(f"Result Image {i+1}", result_img)
+
+cv.waitKey(0)
+cv.destroyAllWindows()
