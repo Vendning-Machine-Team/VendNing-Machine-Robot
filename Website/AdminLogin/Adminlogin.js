8c8a19aae281d14d7581007930c3252dcdbc66b2
impr1nce <ppatel9114@gmail.com>
2025-11-05 23:16:39 -0600

    deleted the files on tri's page

---
+++ Website/AdminLogin/Adminlogin.js
@@ -0,0 +1,5 @@
+const submitButton = document.querySelector("#submit-button");
+
+submitButton.addEventListener('click', function(){
+    window.location.replace('../adminPage/adminPage.html');
+});
