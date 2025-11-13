e9a09adfd0772fa68f52ccf329a7d77dba41ac8d
impr1nce <ppatel9114@gmail.com>
2025-11-12 21:23:08 -0600

    made the frontend of all of the pages look similar and removed unwanted files from admin login

--- Website/Payment/payment.js
+++
@@ -1,5 +0,0 @@
-const completeButton = document.querySelector("#complete-payment-button");
-
-completeButton.addEventListener("click", () => {
-    window.location.replace("../TemporaryCode/code.html");
-})
